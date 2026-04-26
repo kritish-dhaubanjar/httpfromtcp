7867bdbf30923e99012084a4b359712df086835b
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2026-04-26 11:26:03 +0545

    feat: Newlines
    Let's update our code to continue to read 8 bytes at a time, but now
    let's format the output line by line. It should now print the data in
    the same format:
    
    read: %s\n
    
    But now %s represents an entire line of the input file. We're decoupling
    how we present and parse (full lines) the data from how we read it (8
    bytes at a time).

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -14,6 +15,8 @@ func main() {
 		log.Fatal(err)
 	}
 
+	str := ""
+
 	for {
 		data := make([]byte, 8)
 
@@ -23,6 +26,19 @@ func main() {
 			break
 		}
 
-		fmt.Printf("read: %s\n", string(data[:n]))
+		data = data[:n]
+
+		if i := bytes.IndexByte(data, '\n'); i != -1 {
+			str += string(data[:i])
+			data = data[i+1:]
+			fmt.Printf("read: %s\n", str)
+			str = ""
+		}
+
+		str += string(data)
+	}
+
+	if len(str) != 0 {
+		fmt.Printf("read: %s\n", str)
 	}
 }
